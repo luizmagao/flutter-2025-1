16e525a009d178a991c6e8584c4d7c0c0d9536b6
Luiz Lins <luizmagao@gmail.com>
2025-03-13 14:56:55 -0300

    🎨 Utilizando o Stack

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -7,42 +7,57 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // return Scaffold(body: Center(child: Text("Sistema de gestão de contas")));
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/logo.png', width: 120),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 32),
-                Text(
-                  "Sistema de gestão de contas",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 32),
-                ),
-                SizedBox(height: 32),
-                TextFormField(
-                  decoration: InputDecoration(label: Text("E-mail")),
-                ),
-                SizedBox(height: 15),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(label: Text("Senha")),
-                ),
-                SizedBox(height: 32),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(Color(0xFFFFA902)),
-                  ),
-                  onPressed: () {},
-                  child: Text("Entrar", style: TextStyle(color: Colors.black)),
+      body: Stack(
+        children: [
+          Image.asset('assets/images/banner.png'),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset('assets/images/stars.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 128),
+                Image.asset('assets/images/logo.png', width: 120),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: 32),
+                    Text(
+                      "Sistema de gestão de contas",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 32),
+                    ),
+                    SizedBox(height: 32),
+                    TextFormField(
+                      decoration: InputDecoration(label: Text("E-mail")),
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(label: Text("Senha")),
+                    ),
+                    SizedBox(height: 32),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                          Color(0xFFFFA902),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "Entrar",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
