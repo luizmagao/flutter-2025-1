cd946562892b639570e882f7552451925f2d51dc
Luiz Lins <luizmagao@gmail.com>
2025-03-13 14:50:26 -0300

    🎨 Adicionando imagens

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -11,27 +11,35 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(32),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
-              "Sistema de gestão de contas",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 32),
-            ),
-            SizedBox(height: 32),
-            TextFormField(decoration: InputDecoration(label: Text("E-mail"))),
-            SizedBox(height: 15),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(label: Text("Senha")),
-            ),
-            SizedBox(height: 32),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Color(0xFFFFA902)),
-              ),
-              onPressed: () {},
-              child: Text("Entrar", style: TextStyle(color: Colors.black)),
+            Image.asset('assets/images/logo.png', width: 120),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 32),
+                Text(
+                  "Sistema de gestão de contas",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 32),
+                ),
+                SizedBox(height: 32),
+                TextFormField(
+                  decoration: InputDecoration(label: Text("E-mail")),
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(label: Text("Senha")),
+                ),
+                SizedBox(height: 32),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Color(0xFFFFA902)),
+                  ),
+                  onPressed: () {},
+                  child: Text("Entrar", style: TextStyle(color: Colors.black)),
+                ),
+              ],
             ),
           ],
         ),
