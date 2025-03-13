060984e94ba564f95a660c329707f09548cd6497
Luiz Lins <luizmagao@gmail.com>
2025-03-13 10:35:55 -0300

    🎨 Construindo nossa aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const BancoDouroApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class BancoDouroApp extends StatelessWidget {
+  const BancoDouroApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    // return const Text("Sistema de gestão de contas");
+    return MaterialApp(home: const Text("Sistema de gestão de contas"));
   }
 }
