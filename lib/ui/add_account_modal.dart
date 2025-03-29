4a467e33b0cb26ba0f17b5961957900e98bc598d
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-03-28 22:37:55 -0300

    🎨 Criando uma modal

---
+++ lib/ui/add_account_modal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AddAccountModal extends StatelessWidget {
+  const AddAccountModal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,11 +1,25 @@
 import 'package:bancodouro/models/account.dart';
 import 'package:bancodouro/services/account_service.dart';
+import 'package:bancodouro/ui/add_account_modal.dart';
 import 'package:bancodouro/ui/styles/app_colors.dart';
 import 'package:bancodouro/ui/widgets/account_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Future<List<Account>> _futureGetAll = AccountService().getAll();
+
+  Future<void> refreshGetAll() async {
+    setState(() {
+      _futureGetAll = AccountService().getAll();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,34 +34,49 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return const AddAccountModal();
+            },
+          );
+        },
+        backgroundColor: AppColors.orange,
+        child: Icon(Icons.add, color: Colors.black),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: FutureBuilder(
-          future: AccountService().getAll(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return const Center(child: CircularProgressIndicator());
-              case ConnectionState.waiting:
-                return const Center(child: CircularProgressIndicator());
-              case ConnectionState.active:
-                return const Center(child: CircularProgressIndicator());
-              case ConnectionState.done:
-                if (snapshot.data == null || snapshot.data!.isEmpty) {
-                  return const Center(child: Text("Nenhum conta encontrada"));
-                }
+        child: RefreshIndicator(
+          onRefresh: refreshGetAll,
+          child: FutureBuilder(
+            future: _futureGetAll,
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return const Center(child: CircularProgressIndicator());
+                case ConnectionState.waiting:
+                  return const Center(child: CircularProgressIndicator());
+                case ConnectionState.active:
+                  return const Center(child: CircularProgressIndicator());
+                case ConnectionState.done:
+                  if (snapshot.data == null || snapshot.data!.isEmpty) {
+                    return const Center(child: Text("Nenhum conta encontrada"));
+                  }
 
-                List<Account> listAccounts = snapshot.data!;
+                  List<Account> listAccounts = snapshot.data!;
 
-                return ListView.builder(
-                  itemCount: listAccounts.length,
-                  itemBuilder: (context, index) {
-                    Account account = listAccounts[index];
-                    return AccountWidget(account: account);
-                  },
-                );
-            }
-          },
+                  return ListView.builder(
+                    itemCount: listAccounts.length,
+                    itemBuilder: (context, index) {
+                      Account account = listAccounts[index];
+                      return AccountWidget(account: account);
+                    },
+                  );
+              }
+            },
+          ),
         ),
       ),
     );
